6ffbb918bc3a77d5a2e80ce9d5cb2f524be33d6a
evamode29 <maghighi9201@gmail.com>
2026-09-06 19:28:15 +0330

    Fix conflicting diagnostic migration branches

---
+++ exams/migrations/0005_merge_diagnostic_migrations.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("exams", "0003_add_diagnostic_topics"),
+        ("exams", "0003_placement_answer_key_approval"),
+        ("exams", "0004_seed_diagnostic_topics"),
+    ]
+
+    operations = []
