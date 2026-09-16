3e6fed373060bb630d817d8bf4ef618b516d80ab
evamode29 <maghighi9201@gmail.com>
2026-09-16 19:14:00 +0330

    Fix conflicting core migration branches

---
+++ core/migrations/0004_merge_20260914.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("core", "0003_create_teacher_account"),
+        ("core", "0002_rename_core_classr_grade_i_2a4d25_idx_core_classr_grade_e39fb1_idx_and_more"),
+    ]
+
+    operations = []
