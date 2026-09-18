27377dfb78708d89a4a0f5042d7fb2cb55c417d5
evamode29 <maghighi9201@gmail.com>
2026-09-18 13:34:19 +0330

    Fix students migration graph conflict

---
+++ students/migrations/0005_merge_20260918_mobile_xp.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("students", "0004_rename_students_stu_xp_8d4f7b_idx_students_st_xp_2caca1_idx"),
+        ("students", "0003_optional_student_mobile"),
+    ]
+
+    operations = []
